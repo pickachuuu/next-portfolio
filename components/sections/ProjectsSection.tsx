4f47f8260bc1544e3bce1d596035e596f953f4bc
Derek <dberb789@gmail.com>
2025-05-12 16:15:45 +0800

    feat: update GitHub link and project description in ProjectsSection to reflect new profile and provide clearer context

--- components/sections/ProjectsSection.tsx
+++ components/sections/ProjectsSection.tsx
@@ -3,7 +3,7 @@ import AnimationContainer from '../utils/AnimationContainer';
 import SectionContainer from '../utils/SectionContainer';
 import TitleSectionPageContainer from '../utils/TitleSectionPageContainer';
 
-const myGithub = 'https://github.com/jeandv';
+const myGithub = 'https://github.com/pickachuuu';
 
 const ProjectsSection = () => {
   return (
@@ -16,7 +16,7 @@ const ProjectsSection = () => {
         <AnimationContainer customClassName='w-full flex flex-col gap-5 mb-8'>
 
           <p className='w-full text-base text-gray-400'>
-            These are most of the projects I've done since I started programming, some of them are personal projects, freelance, work, practice or for other situation. If you want to see absolutely all my projects go to my <a href={myGithub} target='_blank' rel='noopener noreferrer' className='hover:text-white hover:underline transition-all ease'>github.com/jeandv</a>.
+            Here are some of my projects, including school, passion, and client work. To see all my projects, visit my <a href={myGithub} target='_blank' rel='noopener noreferrer' className='hover:text-white hover:underline transition-all ease'>github.com/pickachuuu</a>.
           </p>
 
         </AnimationContainer>
